02766d4952e8fb1ef87947609970822c12d2cad3
fira <loyauflorian@gmail.com>
2011-12-30 20:45:57 +0100

    Rewrote comments in utils/config.php

--- website/utils/config.php
+++ website/utils/config.php
@@ -1,30 +1,23 @@
 <?php	
 	/* utils/config.php 
-		This file is designed to load a XML File, and parse it to set
-		configuration variables used in the rest of the website
-		Note that because it parses an XML File, it's advised to load it 
-		only where neccessary to reduce server load */
+		Loads configuration variables from an XML File using SimpleXML
+		By default that file should be config.xml in root directory
+    */
 
-	// Hardcoded variable for the path to the config file. Unless you have a better idea.
-	$configFile = 'config.xml';
-
-
-	/* Let's keep it simple. It's just a configuration file, we don't need
-	fancy data trees. We'll process every tag at the root level and 
-	set its value in the configuration array, with key being the tag name. 
-	IE. if you have <stuff>things</stuff> it'll set $_CONFIG['stuff'] = things 
-	Why not even use just a space or comma separated file ?? */
+	/* This is just a config file, so let's keep it simple.
+	   We don't need to do fancy trees, we'll just associate every tag name
+	   to it's value in $_CONFIG.
+	   EG. <tag>content</tag>    --->    $_CONFIG['tag'] = "content"
+	*/
 	
-	/* We can't directly use the path, the function takes a path relative to the including file
-	 We then get the absolute path to config/config.php instead, 
-	and go up one directory, to end up in the site root */
-	$configDoc = simplexml_load_file(__DIR__ . '/../' . $configFile);
+	/* Note that we need to shift the relative path to an absolute path
+	   because the opening is done relative to the file processed by PHP */
+	$configDoc = simplexml_load_file(__DIR__ . '/../config.xml');
 	foreach($configDoc->children() as $item) {
 		$_CONFIG[$item->getName()] = $item;
 	}
 
 	/* Lets clean up the variables used so they don't annoy us in other pages */
 	unset($configDoc);
-	unset($configPath);
 	unset($item);
 ?>
