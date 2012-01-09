2eb5c38f5360f418a4b371c0d3b96eb5e58a6fe3
fira <loyauflorian@gmail.com>
2012-01-09 15:07:36 +0100

    Made the main page much cooler!

--- website/parts/main.php
+++ website/parts/main.php
@@ -6,18 +6,18 @@
 	if(!isset($_SESSION['userid'])) { ?>
 		<div id="reg-dialog" title="Signing up"></div>
 		<div id="login-dialog" title="Signing in"></div>
-		<button id="reg-button">Sign up</button>
-		<button id="login-button">Sign in</button>
+		<button class="floatbutton" id="reg-button">Sign up</button>
+		<button class="floatbutton" id="login-button">Sign in</button>
+
+		You are not logged in! Please login with the buttons at the top of the page
+
 	<?php } else { ?>
-		<button id="logout-button">Log out</button><br /><br />
-	<?php } 
-	
-	/* Actual page body - to be displayed only to registered users */ 
-	if(isset($_SESSION['userid'])) {	?>
+		<button id="logout-button" class="floatbutton">Log out</button><br />
+		<?php widget_infobox("Currently logged in as: ". $_SESSION['username'], true, "userinfobox"); ?><br />
 				
-		<fieldset style="overflow: hidden; float: left;">
+		<fieldset class="mainfieldset">
 			<legend>Upload a file</legend>
-			<div id="upload" title="File Upload">
+			<div id="upload">
 				<form enctype="multipart/form-data" action="parts/uploader.php" method="POST">
 					<table><tr><td>
 					<input type="hidden" name="MAX_FILE_SIZE" value="100000" />
@@ -31,7 +31,7 @@
 						<option value="picture">Picture (JPEG, PNG...)</option>
 					</select>
 					<input name="uploadedfile" type="file"/></td>
-					<td rowspan="2"><input type="image" alt="Upload" width="158" height="45" src="images/upload/upload_button.png"/></td>
+					<td rowspan="2"><input type="image" alt="Upload" width="158" height="45" src="images/upload/upload_button.png" /></td>
 					</tr>
 					<tr><td>
 					<label for="desc">Description: </label>
@@ -42,8 +42,20 @@
 			</div>
 		</fieldset>
 
-		<?php } else { ?>
-		You are not logged in! Please login with the buttons at the top of the page
-		<?php } 
+		<br /><br />
 
-?>
+		<fieldset class="mainfieldset">
+			<legend>Search for files</legend>
+			<form method="POST" action="parts/search.php">
+				<table width="100%"><tr>
+					<td><?php insertField("text"); ?></td>
+					<div id="searchradio">
+						<td><input type="radio" id="searchradio-all" name="selection" value="all" checked="checked" /><label for="searchradio-all">All files</label></td>
+						<td><input type="radio" id="searchradio-user" name="selection" value="user" /><label for="searchradio-user">My Files</label></td>
+					</div>
+					<td><input type="image" alt="Search" src="images/upload/search_button.png" /></td>
+				</tr></table>
+			</form>
+		</fieldset>
+
+<?php } ?>
