46fdc0fb92daac22cad5c37620f08d5698bab104
fira <loyauflorian@gmail.com>
2012-01-09 15:07:21 +0100

    Added an optional class otpion to widgets

--- website/utils/display.php
+++ website/utils/display.php
@@ -11,9 +11,9 @@
 	/* Shows a JQuery Error box widget with the specified content 
 	The big argument encloses the content and icon into a <p> block, effectively
 	adding blank lines at top and bottom of the box. */
-	function widget_errorbox($input, $big=0) { ?>
+	function widget_errorbox($input, $big=0, $opt="") { ?>
 		<div class="ui-widget">
-			<div class="ui-state-error ui-corner-all" style="padding: 0 .7em;"> 
+			<div class="ui-state-error ui-corner-all <?php echo $opt; ?>" style="padding: 0 .7em;"> 
 				<?php if($big) echo "<p>"; ?>
 				<span class="ui-icon ui-icon-alert" style="float: left; margin-right: .3em;"></span> 
 				<?php echo $input; 
@@ -25,9 +25,9 @@
 	
 	/* Shows a JQuery Info box widget with the specified content 
 	See previous function */
-	function widget_infobox($input, $big=false) { ?>
+	function widget_infobox($input, $big=false, $opt="") { ?>
 		<div class="ui-widget">
-			<div class="ui-state-highlight ui-corner-all" style="margin-top: 10px; padding: 0 .4em;"> 
+			<div class="ui-state-highlight ui-corner-all <?php echo $opt; ?>" style="margin-top: 10px; padding: 0 .4em; "> 
 				<?php if($big) echo "<p>"; ?>
 				<p><span class="ui-icon ui-icon-info" style="float: left; margin-right: .2em;"></span>
 				<?php echo $input; 
