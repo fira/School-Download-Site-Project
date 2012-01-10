8728353385ad4f04552dfb0b13f339ebbe6c48ac
fira <loyauflorian@gmail.com>
2012-01-10 16:57:15 +0100

    Fixed a bunch of stuff in the uploader

--- website/parts/uploader.php
+++ website/parts/uploader.php
@@ -5,39 +5,50 @@
 	of the files in an array and save the uploaded files in the /uploads directory
 	*/
 	
+	require_once("../utils/config.php");
 	require_once("../utils/database.php");
-	
-	// Where the file is going to be placed 
-	
-	$target_path = "uploads/";
 
+	// Where the file is going to be placed 
+	$target_path = __DIR__ . "/../uploads/";
+	
 	/* Add the original filename to our target path.  
 	Result is "uploads/filename.extension" */
 	
-	$target_path = $target_path . basename( $_FILES['uploadedfile']['name']); 
+	$userid = $_SESSION['userid'];						
+	$filename = $_FILES['uploadedfile']['name'];		// The file's original name (as sent by the user)
+	$filesize = $_FILES['uploadedfile']['size'];		// The file size in bytes
+	$filetmp = $_FILES['uploadedfile']['tmp_name'];		// The file's current name in the temporary directory
+	$filetype = $_FILES['uploadedfile']['type'];		// The uploaded file's MIME type 
+
+	/* FIXME We should check if the file's not big enough before proceeding */
 
 	/* Check if a file (with its description and category) was browsed to be uploaded */
+	if(!isset($_POST['desc'])) $_POST['desc'] = "";
 	
-	if(isset($_POST['file_cat'], $_POST['desc'] && (($_POST['file_cat']) && $_POST['desc'])) {
-	
-	$exist = oci_parse($db_id, 'SELECT COUNT(name) FROM files WHERE name =' .$_FILES['name']);
-	$same_files = oci_execute($exist)
-	if ($same_files != 0) {
-		$_FILES['name'] = $same_files."-"$_FILES['name'];
-		}
+	db_connect();	
+	$exist = oci_parse($db_id, 'SELECT COUNT(name) FROM files WHERE name=:name');
+	oci_bind_by_name($exist, ':name', $filename);
+	$same_files = oci_execute($exist);
 
-	if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
-		echo "The file ".  basename( $_FILES['uploadedfile']['name']). 
-    " has been uploaded";
-	} else{
+
+	if ($same_files != 0) {
+		$filename = $same_files . "-" . $filename;
+	}
+
+	$target_path = $target_path . basename($filename); 
+
+	/* Constructs the query and send it to the database */	
+	if(move_uploaded_file($filetmp, $target_path)) {
+		echo "The file ".  basename($filename). " has been uploaded";
+	} else {
 		echo "There was an error uploading the file, please try again!";
 	}
 
-	if(!db_connect()) { echo "Error while connecting to the database. Try again later? <br />"; 
-		} else {
-			/* Constructs the query and send it to the database */	
-	
-	$query = oci_parse($db_id, "INSERT INTO files(name,type,filesize,id_user,id_file,upload_date,downloads,description) VALUES('" . $_FILES['name'] . "','" . $_POST['file_cat'] . "','" . $_FILES['size'] . "','" . $_SESSION['userid'] . "', filesIDs.NEXTVAL, '" . date("F j, Y, g:i a") . "', ,'" . $_POST['desc'] . "')");
+	/* FIXME The query just won't work
+	$query = oci_parse($db_id, "INSERT INTO files(name,type,filesize,id_user,id_file,upload_date,downloads,description) VALUES('" . $filename . "','" . $_FILES['uploadedfile']['type'] . "','" . $filesize . "','" . $_SESSION['userid'] . "', filesIDs.NEXTVAL, '" . time() . "', ,'" . $_POST['desc'] . "')"); */
 
+	//$query = oci_parse($db_id, "INSERT INTO files(id_file, id_user, name, type, filesize, upload_date, downloads, description) VALUES (filesIDs.NEXTVAL, $userid, :name, :type, "
+	//$result = oci_execute($query);
 
+	db_close();
 ?>
